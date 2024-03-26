c23d9efbba2828ec31eb5e2c0fcb0048b116eb4e
blazsmaster <64538277+blazsmaster@users.noreply.github.com>
2024-03-26 01:28:11 +0100

    Added 2 API methods

--- src/lib/GlobalApiV1.ts
+++ src/lib/GlobalApiV1.ts
@@ -1,21 +1,69 @@
 import axios, { AxiosError } from 'axios';
 import { API, Endpoints } from '../api';
-import InstituteSchema from '../models/GlobalApi/InstituteSchema';
+import InstituteDto from '../models/GlobalApi/InstituteDto';
+import InstituteV1Dto from '../models/GlobalApi/InstituteV1Dto';
 
 export class GlobalApiV1 {
-	public async getInstitutes(): Promise<Array<InstituteSchema>> {
+	private readonly api_key: string = '7856d350-1fda-45f5-822d-e1a2f3f1acf0';
+
+	/**
+	 * @description Globális konfigurációs leírók lekérése
+	 */
+	public getGlobalConfigurationDescriptors(): Promise<Record<string, string>> {
 		return new Promise(async (resolve, reject) => {
 			try {
-				const response = await axios.get(API.Global.Host + API.Global.Path + Endpoints.Global.GetInstitutes);
+				const response = await axios.get(API.Global.WinCore + API.Global.Path + Endpoints.Global.GetConfigurationDescriptors);
+				resolve(response.data);
+			} catch (e) {
+				reject((e as AxiosError).response?.data);
+			}
+		});
+	}
 
-				const institutes: Array<InstituteSchema> = [];
+	/**
+	 * @description Intézmények lekérése
+	 */
+	public getInstitutesV1(): Promise<Array<InstituteV1Dto>> {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const response = await axios.get((await this.getGlobalConfigurationDescriptors())['GlobalMobileApiUrlPROD'] + '/api/v1/Institute', {
+					headers: {
+						'apiKey': this.api_key,
+					},
+				});
+
+				const institutes: Array<InstituteV1Dto> = [];
 				for (const institute of response.data) {
-					institutes.push(new InstituteSchema(institute));
+					institutes.push(new InstituteV1Dto(institute));
 				}
 
 				resolve(institutes);
 			} catch (e) {
-				reject((e as AxiosError).response?.data || (e as AxiosError).message);
+				reject((e as AxiosError).response?.data);
+			}
+		});
+	}
+
+	/**
+	 * @description Intézmények lekérése
+	 */
+	public getInstitutes(): Promise<Array<InstituteDto>> {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const response = await axios.get(API.Global.Host + API.Global.Path + Endpoints.Global.GetInstitutes, {
+					headers: {
+						'api-version': 'v1',
+					},
+				});
+
+				const institutes: Array<InstituteDto> = [];
+				for (const institute of response.data) {
+					institutes.push(new InstituteDto(institute));
+				}
+
+				resolve(institutes);
+			} catch (e) {
+				reject((e as AxiosError).response?.data);
 			}
 		});
 	}
