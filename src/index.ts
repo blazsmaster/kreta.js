83ae0696b2e3af7bdf2c9dcdb4c61c53f401ca46
blazsmaster <64538277+blazsmaster@users.noreply.github.com>
2024-03-26 01:26:41 +0100

    Created KretaInsult
    https://github.com/stifolder/kretainsult/blob/master/src/assets/dirtywords.xml

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { GlobalApiV1 } from './lib/GlobalApiV1';
 export { IdpApiV1 } from './lib/IdpApiV1';
 export { MobileApiV3 } from './lib/MobileApiV3';
+export { NaploApiV3 } from './lib/NaploApiV3';
+export { EAdminApiV1 } from './lib/EAdminApiV1';
+export { createKretaInsult } from './lib/KretaInsult';
 export * from './types';
---
+++ src/lib/KretaInsult.ts
@@ -0,0 +1,20 @@
+import raw from '../assets/DirtyWords.json';
+
+interface Item {
+	type: number;
+	text: string;
+}
+
+/**
+ * @description Készít egy véletlenszerű sértést a Kréta DirtyWords.xml fájl alapján
+ * @param prefix - A sértés elé illesztendő szöveg (alap: "Te")
+ */
+export const createKretaInsult = (prefix?: string) => {
+	const p = prefix || 'Te';
+	const txts = raw as Array<Item>;
+	const a = txts.filter((txt: any) => txt.type === 1);
+	const n = txts.filter((txt: any) => txt.type === 2);
+
+	const random = (arr: Array<Item>) => arr[Math.floor(Math.random() * arr.length)];
+	return `${p} ${random(a).text.toLowerCase()} ${random(n).text.toLowerCase()}!`.trim();
+};
