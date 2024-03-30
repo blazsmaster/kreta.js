a44388f08c2bfa9f569c7253f32dbdbc49f177d4
blazsmaster <64538277+blazsmaster@users.noreply.github.com>
2024-03-30 02:49:00 +0100

    Minor description change

--- src/lib/KretaInsult.ts
+++ src/lib/KretaInsult.ts
@@ -6,14 +6,14 @@ interface Item {
 }
 
 /**
- * @description Készít egy véletlenszerű sértést a Kréta DirtyWords.xml fájl alapján
+ * @description Készít egy véletlenszerű sértést a híres Kréta DirtyWords.xml fájl alapján
  * @param prefix - A sértés elé illesztendő szöveg (alap: "Te")
  */
 export const createKretaInsult = (prefix?: string) => {
 	const p = prefix || 'Te';
 	const txts = raw as Array<Item>;
-	const a = txts.filter((txt: any) => txt.type === 1);
-	const n = txts.filter((txt: any) => txt.type === 2);
+	const a = txts.filter((txt: Item) => txt.type === 1);
+	const n = txts.filter((txt: Item) => txt.type === 2);
 
 	const random = (arr: Array<Item>) => arr[Math.floor(Math.random() * arr.length)];
 	return `${p} ${random(a).text.toLowerCase()} ${random(n).text.toLowerCase()}!`.trim();
